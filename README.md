e5d2c1efae552fbec1fe7a56e32d71d4d41050b4
Chamlay Oleg <oleg_ch70@mail.ru>
2019-08-06 10:12:44 +0300

    move content to files - html, js, css

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # esp8266_switch_html_simple
+
+install in Arduino IDE uploader tool for esp8266
+   ToDo - add link to Github repository
+
+copy files from directory "data" to esp8266 board - Menu / Tools /ESP8266 Sketch Data Upload
+
+compile and install to esp8266 board
+
+connect to WiFi "EspXYZ"
+
+open page http://192.168.4.1
--- SimpleAuthentication.ino
+++ SimpleAuthentication.ino
@@ -1,6 +1,7 @@
 #include <ESP8266WiFi.h>
 #include <WiFiClient.h>
 #include <ESP8266WebServer.h>
+#include <FS.h>
 
 #include "WIFI_info.h" //Our HTML webpage contents
 
@@ -9,13 +10,20 @@
 #define STAPSK  ""
 #endif
 
-#define  version_info 3
+#ifndef APSSID
+#define APSSID "ESPap"
+#define APPSK  "pass123"
+#endif
 
-#include "LOGIN_page.h" //Our HTML webpage contents
-#include "MAIN_page.h" //Our HTML webpage contents
+#define  version_info 4
 
 const char* ssid = STASSID;
 const char* password = STAPSK;
+
+const char* ap_ssid = APSSID;
+const char* ap_password = APPSK;
+
+bool storedConfigurationExists = false;
 
 ESP8266WebServer server(80);
 
@@ -33,6 +41,43 @@ bool is_authenticated() {
   }
   Serial.println("Authentication Failed");
   return false;
+}
+
+String getContentType(String filename) {
+    if (server.hasArg("download")) return "application/octet-stream";
+    else if (filename.endsWith(".html")) return "text/html";
+    else if (filename.endsWith(".css")) return "text/css";
+    else if (filename.endsWith(".js")) return "application/javascript";
+    else if (filename.endsWith(".png")) return "image/png";
+    else if (filename.endsWith(".gif")) return "image/gif";
+    else if (filename.endsWith(".jpg")) return "image/jpeg";
+    else if (filename.endsWith(".svg")) return "image/svg+xml";
+    else if (filename.endsWith(".ico")) return "image/x-icon";
+    else if (filename.endsWith(".pdf")) return "application/x-pdf";
+    else if (filename.endsWith(".zip")) return "application/x-zip";
+    else if (filename.endsWith(".gz")) return "application/x-gzip";
+    return "text/plain";
+}
+
+bool handleFileRead(String path) {
+    String contentType = getContentType(path);
+    String pathWithGz = path + ".gz";
+    Serial.print("handle file ");
+    Serial.print(path);
+
+    if (SPIFFS.exists(pathWithGz) || SPIFFS.exists(path)) {
+        if (SPIFFS.exists(pathWithGz)) {
+            path += ".gz";
+            Serial.print(".gz");
+        }
+        File file = SPIFFS.open(path, "r");
+        size_t sent = server.streamFile(file, contentType);
+        file.close();
+        Serial.println(" success");
+        return true;
+    }
+    Serial.println(" failed");
+    return false;
 }
 
 //login page, also called for disconnect
@@ -69,9 +114,17 @@ void handleLogin() {
   // content += "<input type='submit' name='SUBMIT' value='Submit'></form>" + msg + "<br>";
   // content += "You also can go <a href='/inline'>here</a></body></html>";
 
+/*
   String content = LOGIN_page;
 
   server.send(200, "text/html", content);
+*/
+
+    if ( ! handleFileRead("/login.html")) {
+        server.send(404, "text/plain", "FileNotFound");
+        return;
+    }
+
 }
 
 //root page can be accessed only if authentication is ok
@@ -82,6 +135,7 @@ void handleRoot() {
     server.sendHeader("Location", "/login");
     server.sendHeader("Cache-Control", "no-cache");
     server.send(301);
+    Serial.println("redirect to /login");
     return;
   }
   // String content = "<html><body><H2>hello, you successfully connected to esp8266!</H2><br>";
@@ -89,8 +143,12 @@ void handleRoot() {
   //   content += "the user agent used is : " + server.header("User-Agent") + "<br><br>";
   // }
   // content += "You can access this page until you <a href=\"/login?DISCONNECT=YES\">disconnect</a></body></html>";
-  String content = MAIN_page;
-  server.send(200, "text/html", content);
+
+    if ( ! handleFileRead("/main.html")) {
+        server.send(404, "text/plain", "FileNotFound");
+        return;
+    }
+
 }
 
 //no need authentication
@@ -148,6 +206,12 @@ void handleSwitchPost() {
 
 
 void handleNotFound() {
+
+    if ( handleFileRead(server.uri())) {
+        return;
+    }
+
+  
   String message = "File Not Found\n\n";
   message += "URI: ";
   message += server.uri();
@@ -160,6 +224,38 @@ void handleNotFound() {
     message += " " + server.argName(i) + ": " + server.arg(i) + "\n";
   }
   server.send(404, "text/plain", message);
+  Serial.println(message);
+}
+
+void readStoredConfiguration() {
+
+}
+
+void activateAccessPoint() {
+    Serial.println();
+    Serial.print("Configuring access point...");
+/* You can remove the password parameter if you want the AP to be open. */
+    WiFi.softAP(ap_ssid, ap_password);
+
+    IPAddress myIP = WiFi.softAPIP();
+    Serial.print("AP IP address: ");
+    Serial.println(myIP);
+}
+
+void initFileSystem() {
+  // Инициализация FFS
+ SPIFFS.begin();
+ {
+ Dir dir = SPIFFS.openDir("/");
+  Serial.println("files");
+ while (dir.next()) {
+ String fileName = dir.fileName();
+ size_t fileSize = dir.fileSize();
+  Serial.print(fileName);
+  Serial.print(" ");
+  Serial.println(fileSize);
+ }
+ }
 }
 
 void setup(void) {
@@ -170,29 +266,73 @@ void setup(void) {
   Serial.println();
   Serial.print("setup. Version ");
   Serial.println(version_info);
+
+  initFileSystem();
+
+
+  readStoredConfiguration();
+
   WiFi.persistent(false);
-  WiFi.mode(WIFI_STA);
-  WiFi.begin(ssid, password);
-  Serial.println("");
 
-  // Wait for connection
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
+  if ( ! storedConfigurationExists ) {
+
+      activateAccessPoint();
+
+  } else {
+
+      WiFi.mode(WIFI_STA);
+      WiFi.begin(ssid, password);
+      Serial.println("");
+
+      // Wait for connection
+      int waitCount = 20;
+      while ( WiFi.status() != WL_CONNECTED ) {
+          delay(500);
+          Serial.print(".");
+          waitCount--;
+
+          if ( waitCount <= 0 ) {
+              activateAccessPoint();
+              break;
+          }
+      }
   }
-  Serial.println("");
-  Serial.print("Connected to ");
-  Serial.println(ssid);
-  Serial.print("IP address: ");
-  Serial.println(WiFi.localIP());
 
+  if ( WiFi.status() != WL_CONNECTED ) {
+      Serial.println("");
+      Serial.print("Accesspoint started ");
+      Serial.println(ap_ssid);
+      Serial.print("Accesspoint password ");
+      Serial.println(ap_password);
+      Serial.print("IP address: ");
+      Serial.println(WiFi.localIP());
+  } else {
+      Serial.println("");
+      Serial.print("Connected to ");
+      Serial.println(ssid);
+      Serial.print("IP address: ");
+      Serial.println(WiFi.localIP());
+  }
 
   server.on("/", handleRoot);
   server.on("/login", handleLogin);
+
   server.on("/inline", []() {
     server.send(200, "text/plain", "this works without need of authentication");
   });
 
+//  server.on("/css/bootstrap.min.css", []() {
+//      server.send(200, "text/plain", RES_bootstrap_min_css);
+//  });
+//
+//  server.on("/js/bootstrap.min.js", []() {
+//      server.send(200, "text/plain", RES_bootstrap_min_js);
+//  });
+//
+//  server.on("/js/jquery.min.js", []() {
+//      server.send(200, "text/plain", REST_jquery_min_js);
+//  });
+//
   server.on("/api/v1/state/switch", handleStateSwitch);
 
   server.onNotFound(handleNotFound);
@@ -204,6 +344,7 @@ void setup(void) {
   server.begin();
   Serial.println("HTTP server started");
 }
+
 
 void loop(void) {
   server.handleClient();
